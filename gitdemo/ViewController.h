7ce3341d26095976dc19a3bdf09e16483516f086
Gokul Narasimhan <ngokul3@gmail.com>
2015-07-14 21:17:47 -0400

    Initial Commit

---
+++ gitdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitdemo
+//
+//  Created by Gokul Narasimhan on 7/14/15.
+//  Copyright (c) 2015 Gokul Narasimhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitdemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitdemo
+//
+//  Created by Gokul Narasimhan on 7/14/15.
+//  Copyright (c) 2015 Gokul Narasimhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
